cc8fc2d3964c40602f6b000e908cb5a1e4fa0ef7
Ashish Shinde <ashishrshinde10@gmail.com>
2026-05-19 13:03:53 +0530

    Learned os module and printed some useful stuff

--- scripts/hello_delta.py
+++ scripts/hello_delta.py
@@ -13,8 +13,6 @@ print("=" * 50)
 # This is Day 2 of the git and github training. We will practice basic git commands and build muscle mermory for it.
 import os
 
-import os
-
 print("=== SYSTEM DIAGNOSTICS ===")
 print(f"Working Directory : {os.getcwd()}")
 print(f"OS Type           : {os.name}")
